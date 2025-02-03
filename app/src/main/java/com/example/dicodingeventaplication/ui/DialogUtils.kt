4bc591920060dc27f1dad43dceda76d8869aab63
Taufiqur Rohman <taufiqurrohman132132@gmail.com>
2025-02-03 14:28:45 +0700

    menambahkan fitur search history dan error

---
+++ app/src/main/java/com/example/dicodingeventaplication/ui/DialogUtils.kt
@@ -0,0 +1,34 @@
+package com.example.dicodingeventaplication.ui
+
+import android.app.AlertDialog
+import android.content.Context
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Handler
+import android.os.Looper
+import android.view.LayoutInflater
+import android.widget.TextView
+import com.example.dicodingeventaplication.R
+
+object DialogUtils {
+
+    // puo op tengah layar
+    fun showPopUpErrorDialog(context: Context, message: String?){
+        val dialogView = LayoutInflater.from(context).inflate(R.layout.item_error_popup , null)
+        val dialog = AlertDialog.Builder(context)
+            .setView(dialogView)
+            .setCancelable(false)
+            .create()
+
+        val tvMessege = dialogView.findViewById<TextView>(R.id.tv_error_msg)
+        tvMessege.text = message
+
+        dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        dialog.show()
+
+        // auto dismis setelah 2 detrik
+        Handler(Looper.getMainLooper()).postDelayed({
+            dialog.dismiss()
+        }, 2000)
+    }
+}
---
+++ app/src/main/java/com/example/dicodingeventaplication/ui/search/filterDialog/FilterDialogFragment.kt
@@ -0,0 +1,85 @@
+package com.example.dicodingeventaplication.ui.search.filterDialog
+
+import android.app.Dialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.viewModels
+import com.example.dicodingeventaplication.R
+import com.example.dicodingeventaplication.databinding.FragmentFilterDialogBinding
+import com.example.dicodingeventaplication.databinding.FragmentHomeBinding
+import com.example.dicodingeventaplication.ui.search.viewModel.SearchViewModel
+import com.google.android.material.button.MaterialButton
+
+class FilterDialogFragment : DialogFragment() {//private val onBtnClick: (Int) -> Unit // tidak di sarankan pakai construktor
+    private var _binding: FragmentFilterDialogBinding? = null
+    private val binding get() = _binding!!
+
+    private val viewModel: SearchViewModel by activityViewModels() // ambil dari activity
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val dialog = super.onCreateDialog(savedInstanceState)
+        dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        return dialog
+    }
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        _binding = FragmentFilterDialogBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val buttons = listOf(
+            binding.btnStateAll,
+            binding.btnStateUpcone,
+            binding.btnStateFinish,
+        )
+
+        buttons.forEach{ button ->
+            button.setOnClickListener{
+//                filterViewModel.selectButton(button.id)
+                viewModel.selectButton(button.id)
+                button.setBackgroundColor(
+                    if(button.isSelected){
+                        resources.getColor(R.color.biru_tua)
+                    }else{
+                        resources.getColor(R.color.abu_abu_terang2)
+                    }
+                )
+                dismiss()
+            }
+        }
+
+        // observe pilihan terakhir agar tetap aktif
+        viewModel.selectButton.observe(viewLifecycleOwner){ selectedId ->
+            buttons.forEach{ button ->
+                button.isSelected = (button.id == selectedId)
+//                button.setBackgroundColor(
+//                    if(button.isSelected){
+//                        resources.getColor(R.color.biru_tua)
+//                    }else{
+//                        resources.getColor(R.color.abu_abu_terang2)
+//                    }
+//                )
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+
+}
